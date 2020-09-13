b810ed4dc3987f6c2d1a6062a9f932b9d22e868e
dagreatcode <dagreatcode@gmail.com>
2020-09-13 10:54:37 -0400

    working on js. things we learned in class. i really need to start off with my html

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-var query = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=799c442d6353dfbe875ad9803c2b4a75";
-console.log(query)
+  // Function for dumping the JSON content for each button into the div
+function displayWeatherInfo
+    weather = $(this).attr("weather-data");
+    queryURL = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=799c442d6353dfbe875ad9803c2b4a75";
+    console.log(query);      // This .on("click") function will trigger the AJAX Call
+
+
+
+            $.ajax({
+            url: queryURL,
+            method: "GET"
+            }).then(function(response) {
+            $("#weather-view").text(JSON.stringify(response));
+            });
+        }
+
